d05b46d8dee158863bbbb6253e41c1175bd4f78a
xyzhu <xyzhu1120@gmail.com>
2014-01-30 21:29:41 +0800

    add replytomeitem

---
+++ ReplyToMeItem.h
@@ -0,0 +1,16 @@
+//
+//  MyReplyItem.h
+//  yssy
+//
+//  Created by xyz on 14-1-30.
+//  Copyright (c) 2014年 Rurui Ye. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReplyToMeItem : NSObject
+@property NSString *from;
+@property NSString *board;
+@property NSString *title;
+@property NSDate *replydate;
+@end
